436656e674bbd7289d5179f84543f5b685e85aad
Jay Ferrando <jay@searcheye.io>
2024-04-20 15:09:55 -0400

    v0.1.1 - improved error handling, don't auto-select a MIDI device, improvements to test harness

--- VNyanHelper.cs
+++ VNyanHelper.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using UnityEditor;
 using System.IO;
 using VNyanInterface;
@@ -11,9 +12,83 @@ using System.Runtime.InteropServices.ComTypes;
 
 namespace JayoMidiPlugin
 {
-    public static class VNyanHelper
+    public class VNyanHelper
     {
-        public static void setVNyanParameterFloat(string parameterName, float value)
+        private VNyanTestHarness testHarness;
+        private GameObject testCanvasObject;
+
+        public VNyanHelper()
+        {
+            if ((VNyanInterface.VNyanInterface.VNyanParameter == null))
+            {
+
+                DefaultControls.Resources uiResources = new DefaultControls.Resources();
+                foreach (Sprite sprite in Resources.FindObjectsOfTypeAll<Sprite>())
+                {
+                    if (sprite.name == "UISprite")
+                    {
+                        uiResources.standard = sprite;
+                        break;
+                    }
+                }
+
+                var harnessObject = GameObject.Find("__VNyanTestHarness");
+                if (harnessObject == null)
+                {
+                    Debug.Log($"Instantiating Test Harness");
+                    harnessObject = new GameObject("__VNyanTestHarness");
+                
+                }
+
+                if (GameObject.FindObjectOfType<EventSystem>() == null)
+                {
+                    var eventSystem = new GameObject("EventSystem", typeof(EventSystem), typeof(StandaloneInputModule));
+                }
+
+
+                var canvasObject = GameObject.Find("__VNyanTestHarness/__VNyanTestCanvas");
+                if (canvasObject == null)
+                {
+                    Debug.Log($"Instantiating Test Canvas");
+                    canvasObject = new GameObject("__VNyanTestCanvas");
+                    canvasObject.AddComponent<Canvas>();
+                    Canvas myCanvas = canvasObject.GetComponent<Canvas>();
+                    myCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+                    canvasObject.AddComponent<CanvasScaler>();
+                    canvasObject.AddComponent<GraphicRaycaster>();
+                    canvasObject.transform.SetParent(harnessObject.transform);
+
+                    GameObject canvasButtonHolder = DefaultControls.CreatePanel(uiResources);
+                    canvasButtonHolder.name = "__VNyanPluginButtonHolder";
+                    canvasButtonHolder.transform.SetParent(canvasObject.transform);
+
+                    GameObject canvasParameterHolder = DefaultControls.CreatePanel(uiResources);
+                    canvasParameterHolder.name = "__VNyanParameterHolder";
+                    canvasParameterHolder.transform.SetParent(canvasObject.transform);
+                    canvasParameterHolder.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 12, 468);
+                    canvasParameterHolder.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, 12, 240);
+
+                    GameObject stringParameterScroll = DefaultControls.CreateScrollView(uiResources);
+                    stringParameterScroll.name = "__VNyanStringParameterScroll";
+                    stringParameterScroll.transform.SetParent(canvasParameterHolder.transform);
+                    stringParameterScroll.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 12, 216);
+                    stringParameterScroll.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 12, 216);
+
+                    GameObject floatParameterScroll = DefaultControls.CreateScrollView(uiResources);
+                    floatParameterScroll.name = "__VNyanFloatParameterScroll";
+                    floatParameterScroll.transform.SetParent(canvasParameterHolder.transform);
+                    floatParameterScroll.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 240, 216);
+                    floatParameterScroll.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 12, 216);
+
+                }
+                testCanvasObject = canvasObject;
+
+                harnessObject.AddComponent<VNyanTestHarness>();
+                testHarness = harnessObject.GetComponent<VNyanTestHarness>();
+            }
+        }
+
+        public void setVNyanParameterFloat(string parameterName, float value)
         {
             Debug.Log($"Setting parameter { parameterName } to {value.ToString()}");
             if (!(VNyanInterface.VNyanInterface.VNyanParameter == null))
@@ -21,11 +96,11 @@ namespace JayoMidiPlugin
                 VNyanInterface.VNyanInterface.VNyanParameter.setVNyanParameterFloat(parameterName, value);
             } else
             {
-                findTestHarness().setFloatParameter(parameterName, value);
+                testHarness.setFloatParameter(parameterName, value);
             }
         }
 
-        public static void setVNyanParameterString(string parameterName, string value)
+        public void setVNyanParameterString(string parameterName, string value)
         {
             Debug.Log($"Setting parameter { parameterName } to {value}");
             if (!(VNyanInterface.VNyanInterface.VNyanParameter == null))
@@ -33,33 +108,33 @@ namespace JayoMidiPlugin
                 VNyanInterface.VNyanInterface.VNyanParameter.setVNyanParameterString(parameterName, value);
             } else
             {
-                findTestHarness().setStringParameter(parameterName, value);
+                testHarness.setStringParameter(parameterName, value);
             }
         }
 
-        public static float getVNyanParameterFloat(string parameterName)
+        public float getVNyanParameterFloat(string parameterName)
         {
             if (!(VNyanInterface.VNyanInterface.VNyanParameter == null))
             {
                 return VNyanInterface.VNyanInterface.VNyanParameter.getVNyanParameterFloat(parameterName);
             } else
             {
-                return findTestHarness().getFloatParameter(parameterName);
+                return testHarness.getFloatParameter(parameterName);
             }
         }
 
-        public static string getVNyanParameterString(string parameterName)
+        public string getVNyanParameterString(string parameterName)
         {
             if (!(VNyanInterface.VNyanInterface.VNyanParameter == null))
             {
                 return VNyanInterface.VNyanInterface.VNyanParameter.getVNyanParameterString(parameterName);
             } else
             {
-                return findTestHarness().getStringParameter(parameterName);
+                return testHarness.getStringParameter(parameterName);
             }
         }
 
-        public static GameObject pluginSetup(IButtonClickedHandler pluginInstance, string buttonText, GameObject windowPrefab)
+        public GameObject pluginSetup(IButtonClickedHandler pluginInstance, string buttonText, GameObject windowPrefab)
         {
             // Register button to plugins window
             if (!(VNyanInterface.VNyanInterface.VNyanUI == null))
@@ -69,71 +144,44 @@ namespace JayoMidiPlugin
                 // Create a window that will show when the button in plugins window is clicked
                 return (GameObject)VNyanInterface.VNyanInterface.VNyanUI.instantiateUIPrefab(windowPrefab);
             } else {
+                testHarness.registerPluginButton(buttonText, pluginInstance);
                 Debug.Log($"(Test Mode) Instantiating Window Prefab");
                 GameObject window = GameObject.Instantiate(windowPrefab);
-                window.transform.parent = findTestCanvasObject().transform;
+                window.transform.SetParent(testCanvasObject.transform);
                 return window;
             }
                 
             
         }
 
-        public static Dictionary<string, string> loadPluginSettingsData(string fileName)
+        public Dictionary<string, string> loadPluginSettingsData(string fileName)
         {
             if (!(VNyanInterface.VNyanInterface.VNyanSettings == null))
             {
                 return VNyanInterface.VNyanInterface.VNyanSettings.loadSettings(fileName);
             } else {
-                return findTestHarness().loadPluginSettingsData(fileName);
+                return testHarness.loadPluginSettingsData(fileName);
             }
         }
 
-        public static void savePluginSettingsData(string fileName, Dictionary<string, string> pluginSettingsData)
+        public void savePluginSettingsData(string fileName, Dictionary<string, string> pluginSettingsData)
         {
             if (!(VNyanInterface.VNyanInterface.VNyanSettings == null))
             {
                 VNyanInterface.VNyanInterface.VNyanSettings.saveSettings(fileName, pluginSettingsData);
             } else {
-                findTestHarness().savePluginSettingsData(fileName, pluginSettingsData);
+                testHarness.savePluginSettingsData(fileName, pluginSettingsData);
             }
         }
 
-        private static VNyanTestHarness findTestHarness()
+        private VNyanTestHarness getTestHarness()
         {
-            var foundHarnessObject = GameObject.Find("__VNyanTestHarness");
-            if (foundHarnessObject != null)
-            {
-                Debug.Log($"Found Test Harness");
-                return foundHarnessObject.GetComponent<VNyanTestHarness>();
-            }
-            else
-            {
-                Debug.Log($"Instantiating Test Harness");
-                GameObject harnessObject = new GameObject("__VNyanTestHarness");
-                harnessObject.AddComponent<VNyanTestHarness>();
-                return harnessObject.GetComponent<VNyanTestHarness>();
-            }
+            return testHarness;
         }
 
-        private static GameObject findTestCanvasObject()
+        private GameObject getTestCanvasObject()
         {
-            var foundCanvasObject = GameObject.Find("__VNyanTestCanvas");
-            if (foundCanvasObject != null)
-            {
-                Debug.Log($"Found Test Canvas");
-                return foundCanvasObject;
-            }
-            else
-            {
-                Debug.Log($"Instantiating Test Canvas");
-                GameObject canvasObject = new GameObject("__VNyanTestCanvas");
-                canvasObject.AddComponent<Canvas>();
-                Canvas myCanvas = canvasObject.GetComponent<Canvas>();
-                myCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
-                canvasObject.AddComponent<CanvasScaler>();
-                canvasObject.AddComponent<GraphicRaycaster>();
-                return canvasObject;
-            }
+            return testCanvasObject;
         }
     }
 }
